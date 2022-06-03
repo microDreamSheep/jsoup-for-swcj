ce91e8666d8a7aac35509aa8db14e7a1113544eb
midreamsheep <2493853707@qq.com>
2022-06-03 15:35:24 +0800

    初始化

---
+++ src/main/java/com/midream/sheep/swcj/core/executetool/execute/jsoup/pojo/Jsoup.java
@@ -0,0 +1,35 @@
+package com.midream.sheep.swcj.core.executetool.execute.jsoup.pojo;
+
+import java.util.Arrays;
+
+/**
+ * @author midreamsheep
+ */
+public class Jsoup {
+    @Override
+    public String toString() {
+        return "Jsoup{" +
+                "pas=" + Arrays.toString(pas) +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    private Pa[] pas;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Pa[] getPas() {
+        return pas;
+    }
+
+    public void setPas(Pa[] pas) {
+        this.pas = pas;
+    }
+}
